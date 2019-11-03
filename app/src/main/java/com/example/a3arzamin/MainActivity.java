14fd5c386ccb03de9e41eb79b9f6a26e4950fa30
MohammadReza <mr.abbasi0151@gmail.com>
2019-11-03 16:46:59 +0330

    im abbasi

---
+++ app/src/main/java/com/example/a3arzamin/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.a3arzamin;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.flarebit.flarebarlib.FlareBar;
+import com.flarebit.flarebarlib.Flaretab;
+import com.flarebit.flarebarlib.TabEventObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final FlareBar bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setBarBackgroundColor(Color.parseColor("#FFFFFF"));
+        ArrayList<Flaretab> tabs = new ArrayList<>();
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.inboxb),"Inbox","#FFECB3"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.searchb),"Search","#80DEEA"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.phoneb),"فروشگاه","#B39DDB"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.avatarb),"Profile","#EF9A9A"));
+        tabs.add(new Flaretab(getResources().getDrawable(R.drawable.settingsb),"Settings","#B2DFDB"));
+        bottomBar.setTabList(tabs);
+        bottomBar.setSelectedIndex(2);
+        bottomBar.attachTabs(MainActivity.this);
+        bottomBar.setTabChangedListener(new TabEventObject.TabChangedListener() {
+            @Override
+            public void onTabChanged(LinearLayout selectedTab, int selectedIndex, int oldIndex) {
+                //tabIndex starts from 0 (zero). Example : 4 tabs = last Index - 3
+                Toast.makeText(MainActivity.this,"Tab "+ selectedIndex+" Selected.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        Toast.makeText(this, "mohsmma", Toast.LENGTH_SHORT).show();
+        
+    }
+}
